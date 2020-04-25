0f06b030583688fa4d96e7b699b2fb7e40039d04
garlicgarrison <stevenjang75@yahoo.com>
2020-04-25 13:38:17 -0700

    testing login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,18 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function LoginScreen() {
-  return (
-    <View style={styles.container}>
-      <Text>Login Screen</Text>
-    </View>
-  );
-}
+  return(
+    <div className = 'loginScreen'>
+      <Text>DashBro</Text>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    </div>
+
+    )
+
+};
